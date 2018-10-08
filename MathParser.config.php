e0f0c27ae07a6d19efb5ba768ae9313babcc873e
Bernhard Baumrock <bernhard@baumrock.com>
2018-10-08 11:12:51 +0200

    add showIf, remove comments

--- MathParser.config.php
+++ MathParser.config.php
@@ -35,35 +35,27 @@ class MathParserConfig extends ModuleConfig {
       'label' => __('Names of fields where NOT to use MathParser'),
       'options' => $this->getGetAllowedFields(),
       'description' => __('Select as required'),
+      'showIf' => 'autoload=1',
     ]);
 
     return $inputfields;
   }
 
   /**
-	 * Get and set names of image fields for selection in VPS:Image field settings.
-	 *
-	 * For use in an InputfieldAsmSelect.
-	 *
-	 * @access private
-	 * @param object $asmSelectField AsmSelect field to populate with selectable image fields options.
-	 * @return object $asmSelectField AsmSelect field object populated with selectable image fields options.
-	 *
-	 */
-	private function getGetAllowedFields() {
-
+   * get allowed fields based on the fieldtype
+   *
+   * @return void
+   */
+  private function getGetAllowedFields() {
     $allowedFields = array();
-
-		foreach ($this->wire('fields') as $f) {
-			// @note: using strrchr to account for namespaced classes
-			$baseClass = substr(strrchr('\\'.get_class($f->type), '\\'), 1);
+    foreach ($this->wire('fields') as $f) {
+      // @note: using strrchr to account for namespaced classes
+      $baseClass = substr(strrchr('\\'.get_class($f->type), '\\'), 1);
       if(!in_array($baseClass, array('FieldtypeFloat','FieldtypeInteger','FieldtypeDecimal'))) continue;
       // add selectable allowed fields
       // @todo: language fields ok?
-			$allowedFields[$f->id] = $f->get('label|name');
-		}
-
-		return $allowedFields;
-
-	}
+      $allowedFields[$f->id] = $f->get('label|name');
+    }
+    return $allowedFields;
+  }
 }
